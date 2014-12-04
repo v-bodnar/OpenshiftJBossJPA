344adff5f687f5bb2a8904a2779e19d0b62fa981
oper4 <oper4@192.168.28.198>
2014-12-04 20:28:12 +0200

    Commit from JBoss Tools

--- src/main/java/ua/openshift/test/Servlet.java
+++ src/main/java/ua/openshift/test/Servlet.java
@@ -2,7 +2,14 @@ package ua.openshift.test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.LinkedList;
+import java.util.List;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.servlet.ServletException;
@@ -10,6 +17,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import ua.openshift.entities.User;
 
@@ -30,7 +38,47 @@ public class Servlet extends HttpServlet {
         super();
        
     }
-
+    
+	public static Connection getConnection() {
+		try {
+			InitialContext ic = new InitialContext();
+			Context initialContext = (Context) ic.lookup("java:comp/env");
+			DataSource datasource = (DataSource) initialContext.lookup("java:jboss/datasources/MySQLDS");
+			//DataSource datasource = (DataSource) ic.lookup("java:jboss/datasources/MySQLDS");
+			return datasource.getConnection();
+		} catch (Exception e) {
+			System.out.println(e.getLocalizedMessage());
+			return null;
+		}
+	}
+	protected List<User> parseResultSet(ResultSet rs){
+		LinkedList<User> result = new LinkedList<User>();
+		try {
+			while (rs.next()) {
+				User user = new User();
+				user.setId(rs.getInt("Id"));
+				user.setLogin(rs.getString("login"));
+				user.setName(rs.getString("name"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();;
+		}
+		return result;
+	}
+	
+    public User findByPK(Integer key){
+        List<User> list = null;
+        String sql = "SELECT * FROM  Users  WHERE id = ? ;";
+        try (PreparedStatement statement = getConnection().prepareStatement(sql)) {
+            statement.setInt(1, key);
+            ResultSet rs = statement.executeQuery();
+            list = parseResultSet(rs);
+        } catch (Exception e) {
+            e.printStackTrace();;
+        }
+        
+        return list.iterator().next();
+    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
@@ -38,7 +86,7 @@ public class Servlet extends HttpServlet {
 		PrintWriter pw = response.getWriter();
 		pw.print("Servlet");
 		//User user = em.find(User.class,1);
-		
+		pw.print(findByPK(1).getName());
 		//pw.print(user.getName());
 		pw.flush();
 	}
